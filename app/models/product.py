d936e9e1d11a39e600d823556189ee9d3e9ce0b7
Jędrzej <sypkaj0401@gmail.com>
2022-06-10 09:56:40 +0200

    10-06-2022 JS zadanie dom

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,6 @@
 
-class Product:
-    def _init__():
-        pass
+class Product():
+    def __init__(self, product_id, all_opinions, stats):
+        self.product_id = product_id
+        self.all_opinions = all_opinions
+        self.stats = stats
